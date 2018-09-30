e127c2c852ab1f5069002ef9d8aae19529f0fdc2
yonni12 <johnmarka@gmail.com>
2018-09-30 17:51:55 +0100

    Re-write search module to fit with new `score` module API
    This has completely removed all parallelism, at least for now,
    as I'm not convinced it was actually helping (and made a "zero-
    allocation" algorithm much harder)

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,306 +1,252 @@
-// extern crate crossbeam;
-// extern crate itertools;
+pub struct SearchResult<'c> {
+  pub score: Score,
+  pub candidate: &'c str,
+}
 
-// use std::cmp::Ordering;
-// use std::usize;
+pub type SearchResults<'c> = Vec<SearchResult<'c>>;
 
-// use self::crossbeam::channel;
-// use self::crossbeam::scope as thread_scope;
-// use self::itertools::kmerge;
+pub struct Searcher<'c> {
+  results: SearchResults<'c>,
+}
 
-// use score::{has_match, locate, score, LocateResult, ScoreResult};
+impl<'c> Searcher<'c> {
+  pub fn new(candidates: &[&'c str]) -> Self {
+    let mut results = Vec::with_capacity(candidates.len());
+    for candidate in candidates {
+      results.push(SearchResult {
+        score: SCORE_MIN,
+        candidate: *candidate,
+      });
+    }
+    Self { results }
+  }
 
-// /// Collection of scores and the candidates they apply to
-// pub type ScoreResults<'a> = Vec<ScoreResult<'a>>;
-// /// Collection of scores, locations, and the candidates they apply to
-// pub type LocateResults<'a> = Vec<LocateResult<'a>>;
+  pub fn search(&mut self, query: &str) -> &SearchResults<'c> {
+    search_worker(query, &mut self.results, &search_actual);
+    // Sort highest score first
+    self.results.sort_by(|a, b| {
+      a.score
+        .partial_cmp(&b.score)
+        .unwrap_or(Ordering::Less)
+        .reverse()
+    });
+    &self.results
+  }
+}
 
-// /// Search among a collection of candidates using the given query, returning
-// /// an ordered collection of results (highest score first)
-// pub fn search_score<'src>(
-//   query: &str,
-//   candidates: &[&'src str],
-//   parallelism: usize,
-// ) -> ScoreResults<'src> {
-//   search_internal(query, candidates, parallelism, &score).collect()
-// }
+pub type LocateResults<'c> = Vec<Box<PositionalCandidate<'c>>>;
 
-// /// Search among a collection of candidates using the given query, returning
-// /// an ordered collection of results (highest score first) with the locations
-// /// of the query in each candidate
-// pub fn search_locate<'src>(
-//   query: &str,
-//   candidates: &[&'src str],
-//   parallelism: usize,
-// ) -> LocateResults<'src> {
-//   search_internal(query, candidates, parallelism, &locate).collect()
-// }
+pub struct Locator<'c> {
+  results: LocateResults<'c>,
+}
 
-// fn search_internal<'a, F, T>(
-//   query: &str,
-//   candidates: &[&'a str],
-//   parallelism: usize,
-//   search_fn: &'static F,
-// ) -> Box<Iterator<Item = T> + 'a>
-// where
-//   T: PartialOrd + Sized + Send + 'a,
-//   F: Fn(&str, &'a str) -> T + Sync,
-// {
-//   let parallelism = calculate_parallelism(candidates.len(), parallelism, query.is_empty());
-//   let mut candidates = candidates;
-//   let (sender, receiver) = channel::bounded::<Vec<T>>(parallelism);
+impl<'c> Locator<'c> {
+  pub fn new(candidates: &[&'c str]) -> Self {
+    let mut results = Vec::with_capacity(candidates.len());
+    for candidate in candidates {
+      results.push(Box::new(PositionalCandidate::<'c>::new(candidate)));
+    }
+    Self { results }
+  }
 
-//   if parallelism < 2 {
-//     Box::new(search_worker(candidates, query, search_fn).into_iter())
-//   } else {
-//     thread_scope(|scope| {
-//       let mut remaining_candidates = candidates.len();
-//       let per_thread_count = ceil_div(remaining_candidates, parallelism);
+  pub fn locate(&mut self, query: &str) -> &LocateResults<'c> {
+    search_worker(query, &mut self.results, &locate_actual);
+    // Search by highest score first
+    self.results.sort_by(|a, b| {
+      a.score()
+        .partial_cmp(&b.score())
+        .unwrap_or(Ordering::Less)
+        .reverse()
+    });
+    &self.results
+  }
+}
 
-//       // Create "parallelism" threads
-//       while remaining_candidates > 0 {
-//         // Search in this thread's share
-//         let split = candidates.split_at(if remaining_candidates >= per_thread_count {
-//           remaining_candidates -= per_thread_count;
-//           per_thread_count
-//         } else {
-//           remaining_candidates = 0;
-//           remaining_candidates
-//         });
-//         let sender = sender.clone();
-//         scope.spawn(move || {
-//           sender.send(search_worker(split.0, query, search_fn));
-//         });
+//==============================================================================
 
-//         // Remove that share from the candidate slice
-//         candidates = split.1;
-//       }
+use std::cmp::Ordering;
 
-//       drop(sender);
-//     });
+use score::{score, PositionalCandidate, Score, SCORE_MIN};
 
-//     Box::new(kmerge(receiver))
-//   }
-// }
+fn search_worker<F, R>(query: &str, collection: &mut [R], search_fn: &'static F)
+where
+  F: Fn(&str, &mut R) -> (),
+{
+  for item in collection {
+    search_fn(query, item);
+  }
+}
 
-// // Search among candidates against a query in a single thread
-// fn search_worker<'a, 'b, F, T>(candidates: &'b [&'a str], query: &'b str, search_fn: F) -> Vec<T>
-// where
-//   T: PartialOrd,
-//   F: Fn(&str, &'a str) -> T,
-// {
-//   let mut out = Vec::with_capacity(candidates.len());
-//   for candidate in candidates {
-//     if has_match(&query, candidate) {
-//       out.push(search_fn(&query, candidate));
-//     }
-//   }
-//   out.sort_unstable_by(|result1, result2| result1.partial_cmp(result2).unwrap_or(Ordering::Less));
+fn search_actual(query: &str, result: &mut SearchResult) {
+  result.score = score(query, result.candidate);
+}
 
-//   out
-// }
+fn locate_actual(query: &str, result: &mut Box<PositionalCandidate>) {
+  result.locate(query);
+}
 
-// fn calculate_parallelism(
-//   candidate_count: usize,
-//   configured_parallelism: usize,
-//   empty_query: bool,
-// ) -> usize {
-//   if empty_query {
-//     // No need to do much for no query
-//     return 1;
-//   }
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use score::config::{SCORE_GAP_LEADING, SCORE_MATCH_CONSECUTIVE};
 
-//   // Use a ramp up to avoid unecessarily starting threads with few candidates
-//   let ramped_parallelism = match candidate_count {
-//     n if n < 17 => ceil_div(n, 4),
-//     n if n > 32 => ceil_div(n, 8),
-//     _ => 4,
-//   };
+  #[test]
+  fn search_empty() {
+    let mut sr = Searcher::new(&[]);
 
-//   configured_parallelism
-//     .min(ramped_parallelism)
-//     .min(candidate_count)
-//     .max(1)
-// }
+    {
+      let rs = sr.search("");
+      assert_eq!(0, rs.len());
+    }
+    {
+      let rs = sr.search("test");
+      assert_eq!(0, rs.len());
+    }
+  }
 
-// /// Integer ceiling division
-// fn ceil_div(a: usize, b: usize) -> usize {
-//   (a + b - 1) / b
-// }
+  #[test]
+  fn search_short() {
+    let mut sr = Searcher::new(&["tags"]);
+    let rs = sr.search("");
+    assert_eq!(1, rs.len());
+    assert_eq!("tags", rs[0].candidate);
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
+    let mut sr = Searcher::new(&["ñîƹ♺à"]);
+    let rs = sr.search("♺");
+    assert_eq!(1, rs.len());
+    assert_eq!("ñîƹ♺à", rs[0].candidate);
 
-//   #[test]
-//   fn parallelism_ramp() {
-//     assert_eq!(1, calculate_parallelism(0, 0, false));
-//     assert_eq!(1, calculate_parallelism(1, 0, false));
-//     assert_eq!(1, calculate_parallelism(0, 1, false));
-//     assert_eq!(1, calculate_parallelism(1, 1, false));
+    let mut sr = Searcher::new(&["tags", "test"]);
+    {
+      let rs = sr.search("");
+      assert_eq!(2, rs.len());
+      for r in rs {
+        assert_eq!(r.score, SCORE_MIN);
+      }
+    }
+    {
+      let rs = sr.search("te");
+      assert_eq!("test", rs[0].candidate);
+      assert_eq!("tags", rs[1].candidate);
+      assert_eq!(SCORE_MIN, rs[1].score);
+    }
+    {
+      let rs = sr.search("foobar");
+      assert_eq!(2, rs.len());
+      for r in rs {
+        assert_eq!(r.score, SCORE_MIN);
+      }
+    }
+    {
+      let rs = sr.search("ts");
+      assert_eq!(2, rs.len());
+      for r in rs {
+        assert_ne!(r.score, SCORE_MIN);
+      }
+    }
+  }
 
-//     assert_eq!(1, calculate_parallelism(2, usize::MAX, false));
-//     assert_eq!(1, calculate_parallelism(3, 4, false));
-//     assert_eq!(1, calculate_parallelism(4, 2, false));
+  #[test]
+  fn search_med() {
+    let cs = &[
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+      "twenty",
+    ];
 
-//     for n in 5..9 {
-//       assert_eq!(2, calculate_parallelism(n, usize::MAX, false));
-//       assert_eq!(1, calculate_parallelism(n, usize::MAX, true));
-//     }
+    let mut sr = Searcher::new(cs);
 
-//     for n in 9..13 {
-//       assert_eq!(3, calculate_parallelism(n, usize::MAX, false));
-//       assert_eq!(1, calculate_parallelism(n, usize::MAX, true));
-//     }
+    {
+      let rs = sr.search("");
+      assert_eq!(cs.len(), rs.len());
+    }
+    {
+      let rs = sr.search("teen");
+      assert_eq!(cs.len(), rs.len());
+      let mut iter = rs.iter();
+      let r = iter.next().unwrap();
+      assert_eq!(r.candidate, "fifteen");
+      assert_eq!(
+        r.score,
+        3.0 * SCORE_GAP_LEADING + 3.0 * SCORE_MATCH_CONSECUTIVE
+      );
+      for r in iter.by_ref().take(6) {
+        assert_eq!(
+          "neet",
+          r.candidate.chars().rev().take(4).collect::<String>()
+        );
+        assert!(r.score > 0.0);
+      }
+      for r in iter {
+        assert_eq!(r.score, SCORE_MIN);
+      }
+    }
+    {
+      let rs = sr.search("tee");
+      let mut iter = rs.iter();
+      for r in iter.by_ref().take(7) {
+        assert_ne!(r.score, SCORE_MIN);
+        assert_eq!(
+          "neet",
+          r.candidate.chars().rev().take(4).collect::<String>()
+        );
+      }
+      let r = iter.next().unwrap();
+      assert_ne!(r.score, SCORE_MIN);
+      assert_eq!(r.candidate, "three");
+      let r = iter.next().unwrap();
+      assert_ne!(r.score, SCORE_MIN);
+      assert_eq!(r.candidate, "twelve");
+      for r in iter {
+        assert_eq!(r.score, SCORE_MIN);
+      }
+    }
+    {
+      let rs = sr.search("six");
+      assert_eq!("six", rs[0].candidate);
+    }
+  }
 
-//     for n in 13..33 {
-//       assert_eq!(4, calculate_parallelism(n, usize::MAX, false));
-//       assert_eq!(1, calculate_parallelism(n, usize::MAX, true));
-//     }
+  #[test]
+  fn search_large() {
+    let n = 100_000;
+    let mut candidates = Vec::with_capacity(n);
+    for i in 0..n {
+      candidates.push(format!("{}", i));
+    }
 
-//     for n in 1..10_000 {
-//       assert!(calculate_parallelism(n, 12, false) <= 12);
-//       assert_eq!(1, calculate_parallelism(n, 12, true));
-//     }
-//   }
+    let mut sr = Searcher::new(&(candidates.iter().map(|s| &s[..]).collect::<Vec<&str>>()));
 
-//   fn search_empty_with_parallelism(parallelism: usize) {
-//     let rs = search_score("", &[], parallelism);
-//     assert_eq!(0, rs.len());
+    let rs = sr.search("12");
 
-//     let rs = search_score("test", &[], parallelism);
-//     assert_eq!(0, rs.len());
-//   }
+    // This has been precalculated
+    // e.g. via `$ seq 0 99999 | grep '.*1.*2.*' | wc -l`
+    assert_eq!("12", rs[0].candidate);
+    let mut iter = rs.iter();
+    for r in iter.by_ref().take(8146) {
+      assert_ne!(r.score, SCORE_MIN)
+    }
+    for r in iter {
+      assert_eq!(r.score, SCORE_MIN);
+    }
+  }
 
-//   fn search_with_parallelism(parallelism: usize) {
-//     search_empty_with_parallelism(parallelism);
-
-//     let rs = search_score("", &["tags"], parallelism);
-//     assert_eq!(1, rs.len());
-//     assert_eq!("tags", rs[0].candidate);
-
-//     let rs = search_score("♺", &["ñîƹ♺à"], parallelism);
-//     assert_eq!(1, rs.len());
-//     assert_eq!("ñîƹ♺à", rs[0].candidate);
-
-//     let cs = &["tags", "test"];
-
-//     let rs = search_score("", cs, parallelism);
-//     assert_eq!(2, rs.len());
-
-//     let rs = search_score("te", cs, parallelism);
-//     assert_eq!(1, rs.len());
-//     assert_eq!("test", rs[0].candidate);
-
-//     let rs = search_score("foobar", cs, parallelism);
-//     assert_eq!(0, rs.len());
-
-//     let rs = search_score("ts", cs, parallelism);
-//     assert_eq!(2, rs.len());
-//     assert_eq!(
-//       vec!["test", "tags"],
-//       rs.iter()
-//         .map(|r| r.candidate)
-//         .collect::<Vec<&'static str>>()
-//     );
-//   }
-
-//   fn search_med_parallelism(parallelism: usize) {
-//     let cs = &[
-//       "one",
-//       "two",
-//       "three",
-//       "four",
-//       "five",
-//       "six",
-//       "seven",
-//       "eight",
-//       "nine",
-//       "ten",
-//       "eleven",
-//       "twelve",
-//       "thirteen",
-//       "fourteen",
-//       "fifteen",
-//       "sixteen",
-//       "seventeen",
-//       "eighteen",
-//       "nineteen",
-//       "twenty",
-//     ];
-
-//     let rs = search_score("", cs, parallelism);
-//     assert_eq!(cs.len(), rs.len());
-
-//     let rs = search_score("teen", cs, parallelism);
-//     assert_eq!(7, rs.len());
-//     for r in rs {
-//       assert_eq!(
-//         "neet",
-//         r.candidate.chars().rev().take(4).collect::<String>()
-//       );
-//     }
-
-//     let rs = search_score("tee", cs, parallelism);
-//     assert_eq!(9, rs.len());
-//     assert_eq!(
-//       "neet",
-//       rs[0].candidate.chars().rev().take(4).collect::<String>()
-//     );
-
-//     let rs = search_score("six", cs, parallelism);
-//     assert_eq!("six", rs[0].candidate);
-//   }
-
-//   fn search_large_parallelism(parallelism: usize) {
-//     let n = 100_000;
-//     let mut candidates = Vec::with_capacity(n);
-//     for i in 0..n {
-//       candidates.push(format!("{}", i));
-//     }
-
-//     let rs = search_score(
-//       "12",
-//       &(candidates.iter().map(|s| &s[..]).collect::<Vec<&str>>()),
-//       parallelism,
-//     );
-
-//     // This has been precalculated
-//     // e.g. via `$ seq 0 99999 | grep '.*1.*2.*' | wc -l`
-//     assert_eq!(8146, rs.len());
-//     assert_eq!("12", rs[0].candidate);
-//   }
-
-//   // TODO: test locate
-
-//   #[test]
-//   fn search_single() {
-//     search_with_parallelism(0);
-//     search_with_parallelism(1);
-//     search_large_parallelism(1);
-//   }
-
-//   #[test]
-//   fn search_double() {
-//     search_with_parallelism(2);
-//     search_large_parallelism(2);
-//   }
-
-//   #[test]
-//   fn search_quad() {
-//     search_med_parallelism(4);
-//     search_large_parallelism(4);
-//   }
-
-//   #[test]
-//   fn search_quin() {
-//     search_med_parallelism(4);
-//     search_large_parallelism(5);
-//   }
-
-//   #[test]
-//   fn search_large() {
-//     search_med_parallelism(4);
-//     search_large_parallelism(16);
-//   }
-// }
+  // TODO: test locate
+}
